34a7a897780c3c12a511800f2c5e22f6e187b459
namekeattisak <namekeattisak>
2022-03-25 10:44:04 +0700

    first commit

---
+++ src/testhello/TestHello.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testhello;
+
+/**
+ *
+ * @author Keattisak
+ */
+public class TestHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        System.out.println("Welcome to Thailand");
+        System.out.println("Have a Good Trip");
+        System.out.println("Wait a Minute");
+        System.out.println("Bon Voyage");
+    }
+    
+}
