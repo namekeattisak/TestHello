36984cdf35dc763b4fa27bc2e3b4e1df93eb5f8e
namekeattisak <namekeattisak>
2022-03-25 10:53:59 +0700

    first commit

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -20,6 +20,7 @@ public class TestHello {
         System.out.println("Have a Good Trip");
         System.out.println("Wait a Minute");
         System.out.println("Bon Voyage");
+  
     }
     
 }
